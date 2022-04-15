5ff41ac3c7efbb8ea8d2504259905faca64d78e4
dan634 <dan.caragui@yahoo.com>
2022-04-15 17:48:14 +0300

    file creation update

--- sources/files/main.c
+++ sources/files/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+FILE *f;
+int ret,n;
+void run_action1(void);
 int main()
 {
-    printf("Hello world!\n");
+    printf("Main Start\n");
+
+    run_action1();
+
     return 0;
+}
+
+//Funcions implementation
+void run_action1(void)
+{
+    char s[30];
+    printf("provide full name for the new file:");
+    scanf("%s",&s);
+    printf("\n%s\n",s);
+    f = fopen(s,"wt");
+    fprintf(f,"first line\n");
+    fclose(f);
+    printf("File:%s  created\n",s);
 }
